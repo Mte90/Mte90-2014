7d59472a5cf2fecbcf767aeb7732297c4ed92be5
Mte90 <mte90smanettator@gmail.com>
2015-07-20 12:44:32 +0200

    improvement

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/app.css', false, 'fc667d9ea220be7aeacafd6aed57a0f7');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/app.css', false, '31b718d9f1eaf3a7b01929a2d43cd11a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
