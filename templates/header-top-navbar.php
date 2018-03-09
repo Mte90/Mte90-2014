3c1e2078b6376fa41ea296f401fc658c1d5587e4
Daniele Scasciafratte <mte90net@gmail.com>
2018-03-09 19:31:11 +0100

    added mastodon

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -22,6 +22,9 @@
       <a class="pulse-shrink social-instagram" href="https://instagram.com/Mte90"></a>
     </li>
     <li>
+      <a class="pulse-shrink social-mastodon" href="https://mastodonti.co/@Mte90" target="_blank"></a>
+    </li>
+    <li>
       <a class="pulse-shrink social-youtube" href="https://www.youtube.com/user/Mte90lp" target="_blank"></a>
     </li>
     <li>
